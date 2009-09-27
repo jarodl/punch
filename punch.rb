9f7682b5698eac29ff44b09491387ad50644a2d4
Jarod Luebbert <jarod.luebbert@gmail.com>
2009-09-26 22:20:10 -0500

    Cleaning up.

--- punch.rb
+++ punch.rb
@@ -1,52 +1,10 @@
 require 'sinatra'
-require 'datamapper'
+require 'models'
 require 'json'
 require 'net/http'
 require 'net/https'
 
-DataMapper::setup(:default, ENV['DATABASE_URL'] || 'sqlite3://punch.db')
-
 enable :sessions
-
-class Task
-  include DataMapper::Resource
-  property :id, Serial
-  property :desc, String
-  property :created_at, DateTime
-  property :ended_at, DateTime
-
-  belongs_to :user
-
-  def is_completed?
-    !ended_at.nil?
-  end
-
-  def time_worked
-    worked = self.ended_at - self.created_at
-    hours, minutes = DateTime.day_fraction_to_time(worked)
-    worked = hours + (minutes/60.0)
-  end
-
-end
-
-class User
-  include DataMapper::Resource
-  property :id, Serial
-  property :email, String, :length => 255
-  property :identifier, String, :length => 255
-
-  has n, :tasks
-
-  def self.find(identifier)
-    user = first(:identifier => identifier)
-    user = new(:identifier => identifier) if user.nil?
-    user.save
-    return user
-  end
-
-end
-
-DataMapper.auto_upgrade!
 
 helpers do
 
